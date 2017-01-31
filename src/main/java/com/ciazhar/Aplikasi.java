2acdefc9f52c881441fca093f5ac0980f8b16379
ciazhar <daisetsunakama@gmail.com>
2017-02-01 06:17:26 +0700

    setup project

---
+++ src/main/java/com/ciazhar/Aplikasi.java
@@ -0,0 +1,15 @@
+package com.ciazhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+public class Aplikasi {
+	public static void main(String[] args) {
+		SpringApplication.run(Aplikasi.class, args);
+	}
+
+}
